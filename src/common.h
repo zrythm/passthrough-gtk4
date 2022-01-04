6536a5e3fa4950211ad2604e5aed2950b26e0c7c
Alexandros Theodotou <alex@zrythm.org>
2022-01-04 05:25:04 +0000

    add dsp part

---
+++ src/common.h
@@ -0,0 +1,203 @@
+/*
+ * Copyright (C) 2022 Alexandros Theodotou <alex at zrythm dot org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file
+ *
+ * Common code for both the DSP and the UI.
+ */
+
+#ifndef __PASSTHROUGH_COMMON_H__
+#define __PASSTHROUGH_COMMON_H__
+
+#include "lv2/atom/atom.h"
+#include "lv2/atom/forge.h"
+#include "lv2/core/lv2.h"
+#include "lv2/log/log.h"
+#include "lv2/log/logger.h"
+#include "lv2/midi/midi.h"
+#include "lv2/options/options.h"
+#include "lv2/urid/urid.h"
+#include "lv2/time/time.h"
+#include "lv2/worker/worker.h"
+
+typedef struct PluginUris
+{
+  LV2_URID atom_eventTransfer;
+  LV2_URID atom_Blank;
+  LV2_URID atom_Object;
+  LV2_URID atom_Float;
+  LV2_URID atom_Double;
+  LV2_URID atom_Int;
+  LV2_URID atom_Long;
+  LV2_URID log_Entry;
+  LV2_URID log_Error;
+  LV2_URID log_Note;
+  LV2_URID log_Trace;
+  LV2_URID log_Warning;
+  LV2_URID midi_MidiEvent;
+  LV2_URID time_Position;
+  LV2_URID time_bar;
+  LV2_URID time_barBeat;
+  LV2_URID time_beatsPerMinute;
+  LV2_URID time_beatUnit;
+  LV2_URID time_frame;
+  LV2_URID time_speed;
+
+} PluginUris;
+
+/**
+ * Group of variables needed by all plugins and their
+ * UIs.
+ */
+typedef struct PluginCommon
+{
+  /** Log feature. */
+  LV2_Log_Log *   log;
+
+  /** Map feature. */
+  LV2_URID_Map *  map;
+
+  /** Logger convenience API. */
+  LV2_Log_Logger  logger;
+
+  /** Worker schedule feature. */
+  LV2_Worker_Schedule* schedule;
+
+  /** Atom forge. */
+  LV2_Atom_Forge  forge;
+
+  LV2_Options_Option * options;
+
+  /** URIs. */
+  PluginUris      uris;
+
+  /** Plugin samplerate. */
+  double          samplerate;
+
+} PluginCommon;
+
+typedef enum PortIndex
+{
+  /** GUI to plugin communication. */
+  PASSTHROUGH_CONTROL,
+  /** Plugin to UI communication. */
+  PASSTHROUGH_NOTIFY,
+
+  PASSTHROUGH_CV_IN,
+  PASSTHROUGH_AUDIO_IN,
+  PASSTHROUGH_CONTROL_IN,
+  PASSTHROUGH_CV_OUT,
+  PASSTHROUGH_AUDIO_OUT,
+  PASSTHROUGH_MIDI_OUT,
+  NUM_PASSTHROUGH_PORTS,
+} PortIndex;
+
+static inline void
+map_common_uris (
+  LV2_URID_Map * map,
+  PluginUris *   uris)
+{
+#define MAP(x,uri) \
+  uris->x = map->map (map->handle, uri)
+
+  /* official URIs */
+  MAP (atom_Blank, LV2_ATOM__Blank);
+  MAP (atom_Object, LV2_ATOM__Object);
+  MAP (atom_Float, LV2_ATOM__Float);
+  MAP (atom_Double, LV2_ATOM__Double);
+  MAP (atom_Int, LV2_ATOM__Int);
+  MAP (atom_Long, LV2_ATOM__Long);
+  MAP (atom_eventTransfer, LV2_ATOM__eventTransfer);
+  MAP (log_Entry, LV2_LOG__Entry);
+  MAP (log_Error, LV2_LOG__Error);
+  MAP (log_Note, LV2_LOG__Note);
+  MAP (log_Trace, LV2_LOG__Trace);
+  MAP (log_Warning, LV2_LOG__Warning);
+  MAP (midi_MidiEvent, LV2_MIDI__MidiEvent);
+  MAP (time_Position, LV2_TIME__Position);
+  MAP (time_bar, LV2_TIME__bar);
+  MAP (time_barBeat, LV2_TIME__barBeat);
+  MAP (time_beatsPerMinute, LV2_TIME__beatsPerMinute);
+  MAP (time_beatUnit, LV2_TIME__beatUnit);
+  MAP (time_frame, LV2_TIME__frame);
+  MAP (time_speed, LV2_TIME__speed);
+
+#undef MAP
+}
+
+/**
+ * @param with_worker 1 to add the worker feature.
+ *
+ * @return Non-zero on fail.
+ */
+static inline int
+plugin_common_instantiate (
+  PluginCommon *              self,
+  const LV2_Feature * const * features,
+  int                         with_worker)
+{
+#define HAVE_FEATURE(x) \
+  (!strcmp(features[i]->URI, x))
+
+  for (int i = 0; features[i]; ++i)
+    {
+      if (HAVE_FEATURE (LV2_URID__map))
+        {
+          self->map =
+            (LV2_URID_Map*) features[i]->data;
+        }
+      else if (HAVE_FEATURE (LV2_LOG__log))
+        {
+          self->log =
+            (LV2_Log_Log *) features[i]->data;
+        }
+      else if (HAVE_FEATURE (LV2_OPTIONS__options))
+        {
+          self->options =
+            (LV2_Options_Option*) features[i]->data;
+        }
+
+      if (with_worker)
+        {
+          if (HAVE_FEATURE (LV2_WORKER__schedule))
+            {
+              self->schedule =
+                (LV2_Worker_Schedule *) features[i]->data;
+            }
+        }
+    }
+#undef HAVE_FEATURE
+
+  if (!self->map)
+    {
+      lv2_log_error (
+        &self->logger, "Missing feature urid:map\n");
+      return -1;
+    }
+  else if (with_worker && !self->schedule)
+    {
+      lv2_log_error (
+        &self->logger,
+        "Missing feature work:schedule\n");
+      return -1;
+    }
+
+  return 0;
+}
+
+#endif
---
+++ src/dsp.c
@@ -0,0 +1,206 @@
+/*
+ * Copyright (C) 2022 Alexandros Theodotou <alex at zrythm dot org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "passthrough-config.h"
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "common.h"
+
+typedef struct Passthrough
+{
+  /** Plugin ports. */
+  const LV2_Atom_Sequence * control;
+  LV2_Atom_Sequence *       notify;
+  const float *             cv_in;
+  const float *             audio_in;
+  const float *             control_in;
+
+  /* outputs */
+  float *                   cv_out;
+  float *                   audio_out;
+  LV2_Atom_Sequence *       midi_out;
+
+  PluginCommon              common;
+
+} Passthrough;
+
+static LV2_Handle
+instantiate (
+  const LV2_Descriptor*     descriptor,
+  double                    rate,
+  const char*               bundle_path,
+  const LV2_Feature* const* features)
+{
+  Passthrough * self = calloc (1, sizeof (Passthrough));
+
+  PluginCommon * common = &self->common;
+
+  common->samplerate = rate;
+
+  int ret =
+    plugin_common_instantiate (
+      common, features, 0);
+  if (ret)
+    goto fail;
+
+  /* map uris */
+  map_common_uris (common->map, &common->uris);
+
+  /* init atom forge */
+  lv2_atom_forge_init (
+    &common->forge, common->map);
+
+  /* init logger */
+  lv2_log_logger_init (
+    &common->logger, common->map, common->log);
+
+  return (LV2_Handle) self;
+
+fail:
+  free (self);
+  return NULL;
+}
+
+static void
+connect_port (
+  LV2_Handle instance,
+  uint32_t   port,
+  void *     data)
+{
+  Passthrough * self = (Passthrough*) instance;
+
+  switch ((PortIndex) port)
+    {
+    case PASSTHROUGH_CONTROL:
+      self->control =
+        (const LV2_Atom_Sequence *) data;
+      break;
+    case PASSTHROUGH_NOTIFY:
+      self->notify =
+        (LV2_Atom_Sequence *) data;
+      break;
+    case PASSTHROUGH_CV_IN:
+      self->cv_in = (const float *) data;
+      break;
+    case PASSTHROUGH_AUDIO_IN:
+      self->audio_in = (const float *) data;
+      break;
+    case PASSTHROUGH_CONTROL_IN:
+      self->control_in = (const float *) data;
+      break;
+    default:
+      break;
+    }
+}
+
+static void
+activate (
+  LV2_Handle instance)
+{
+  /*Passthrough * self = (Passthrough *) instance;*/
+}
+
+static void
+run (
+  LV2_Handle instance,
+  uint32_t n_samples)
+{
+  Passthrough * self = (Passthrough *) instance;
+  PluginCommon * common = &self->common;
+
+  /* TODO MIDI */
+#if 0
+  /* write an empty Sequence header to the output */
+  lv2_atom_sequence_clear (self->midi_out);
+  self->midi_out->atom.type = self->control->atom.type;
+#endif
+
+  /* read incoming events from host and UI */
+  LV2_ATOM_SEQUENCE_FOREACH (
+    self->control, ev)
+    {
+      if (lv2_atom_forge_is_object_type (
+            &common->forge, ev->body.type))
+        {
+          const LV2_Atom_Object * obj =
+            (const LV2_Atom_Object*)&ev->body;
+          if (obj->body.otype ==
+                common->uris.time_Position)
+            {
+              /*update_position_from_atom_obj (*/
+                /*&self->common, obj);*/
+              /*xport_changed = 1;*/
+            }
+        }
+    }
+
+  for (uint32_t i = 0; i < n_samples; i++)
+    {
+      self->cv_out[i] = self->cv_in[i];
+      self->audio_out[i] = self->audio_in[i];
+
+    }
+}
+
+static void
+deactivate (
+  LV2_Handle instance)
+{
+}
+
+static void
+cleanup (
+  LV2_Handle instance)
+{
+  free (instance);
+}
+
+static const void*
+extension_data (
+  const char* uri)
+{
+  return NULL;
+}
+
+static const LV2_Descriptor descriptor = {
+  PLUGIN_URI,
+  instantiate,
+  connect_port,
+  activate,
+  run,
+  deactivate,
+  cleanup,
+  extension_data
+};
+
+LV2_SYMBOL_EXPORT
+const LV2_Descriptor*
+lv2_descriptor (
+  uint32_t index)
+{
+  switch (index)
+    {
+    case 0:
+      return &descriptor;
+    default:
+      return NULL;
+    }
+}
